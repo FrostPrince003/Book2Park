41337d754145dcc3b42d0bcea534a82071e087df
Sanjay Kankamwar <132813962+GodriKThEGrafteD@users.noreply.github.com>
2023-05-18 23:20:47 +0530

    Add files via upload

--- parkin.py
+++ parkin.py
@@ -1,76 +1,105 @@
 from tkinter import *
 from tkinter import messagebox
 
-
-
 root=Tk()
-root.title("SignUp")
+root.title("ParkiN")
 root.geometry('925x500+300+200')
 root.configure(bg='#fff')
 root.resizable(False,False)
 
+def book():
+    vehicleno = user.get()
+    phoneno = Phone.get()
+    gmailac = gmail.get()
+    
+
+    if vehicleno == 'M' and phoneno == 'M' and gmailac == 'M' :
+        
+        building=Toplevel(root)
+        building.title('Parkin!')
+        building.geometry('925x500+300+200')
+        building.configure(bg='#fff')
+        building.resizable(False,False)
+    
+        Label(building,text='Please select Your Parking Space',fg='#57a1f8',bg='white',font =('Microsoft Yahei UI Light',20)).place(x=285,y=5)
+                
+        Parking_A = Button(building,text='Parking A',pady=1,width=10,fg='black',border=0,bg='#FFFFEB',font=('Microsoft Yahei UI Light',14,))
+        Parking_A.place(x=140,y=220)
+        
+        op = PhotoImage(file='parkinga.png')
+        op_label = Label(building,image=op,width = 300,height=180,border=0,bg='black').place(x=60,y=40)
+        op_label.image = op
+        
+        Parking_B = Button(building,text='Parking B',pady=1,width=10,fg='black',border=0,bg='#FFFFEB',font=('Microsoft Yahei UI Light',14,))
+        Parking_B.place(x=650,y=220)
+        
+        Parking_C = Button(building,text='Parking C',pady=1,width=10,fg='black',border=0,bg='#FFFFEB',font=('Microsoft Yahei UI Light',14,))
+        Parking_C.place(x=140,y=420)
+        
+        Parking_D = Button(building,text='Parking D',pady=1,width=10,fg='black',border=0,bg='#FFFFEB',font=('Microsoft Yahei UI Light',14,))
+        Parking_D.place(x=650,y=420)
+
+    
+    elif vehicleno!= 'MH34BK2578' and phoneno!= '7028067585' and gmailac!= 'sanjay.22210866@viit.ac.in' :
+        messagebox.showerror("Invalid"," Invalid Vehicle No")
+
 img = PhotoImage(file='parking1.png')
-Label(root,image=img,border=0,bg='white').place(x=-350,y=-280)
+Label(root,image=img,border=0,bg='white').place(x=-350,y=-265)
 
 frame=Frame(root,width=400,height=390,bg='#fff')
 frame.place(x=450,y=80)
 
-heading=Label(frame,text='New User? Sign in!',fg="#57a1f8",bg="white",font=('Microsoft Yahei UI Light',20,))
+heading=Label(frame,text='Welcome to Parkin!',fg="#57a1f8",bg="white",font=('Microsoft Yahei UI Light',20,))
 heading.place(x=120, y=5)
 
-Guest=Button(frame,text='Guest Mode',border=0,fg="#57a1f8",bg="white",font=('Microsoft Yahei UI Light',10))
-Guest.place(x=300, y=350)
-
-Login=Button(frame,text='Already registered? Login',border=0,fg="#57a1f8",bg="white",font=('Microsoft Yahei UI Light',10))
-Login.place(x=100, y=350)
-
-signup=Button(frame,text='Sign Up',pady=1,width=25,fg='White',border=0,bg='#57a1f8',font=('Microsoft Yahei UI Light',14))
-signup.place(x=115,y=260)
-
-
+ok=Button(frame,text='Book',pady=1,width=25,fg='white',border=0,bg='#57a1f8',font=('Microsoft Yahei UI Light',14,),command=book)
+ok.place(x=115,y=250)
 
 def on_enter(e):
     user.delete(0,'end')
 def on_leave(e):
     if user.get()=='':
-        user.insert(0,'Username')
-        
-        
+        user.insert(0,'Vehicle Number ')
+
+
 user=Entry(frame,width=25,fg="black",border=0,bg='white',font=('Microsoft Yahei UI Light',13))
 user.place(x=110,y=67)
-user.insert(0,'Username')
+
+# This input will be given by the parking coordinator
+
+user.insert(0,'Vehicle Number')
 user.bind("<FocusIn>",on_enter)
 user.bind("<FocusOut>",on_leave)
-
 
 Frame(frame,width=295,height=2,bg='black').place(x=110,y=90)
 
 def on_enter(e):
-    passkey.delete(0,'end')
+    Phone.delete(0,'end')
 def on_leave(e):
-    if passkey.get()=='':
-        passkey.insert(0,'Password')
+    if Phone.get()=='':
+        Phone.insert(0,'Phone No.')
 
-passkey=Entry(frame,width=25,fg="black",border=0,bg='white',font=('Microsoft Yahei UI Light',13))
-passkey.place(x=110,y=134)
-passkey.insert(0,'Password')
-passkey.bind("<FocusIn>",on_enter)
-passkey.bind("<FocusOut>",on_leave)
+Phone=Entry(frame,width=25,fg="black",border=0,bg='white',font=('Microsoft Yahei UI Light',13))
+Phone.place(x=110,y=134)
+Phone.insert(0,'Phone No.')
+Phone.bind("<FocusIn>",on_enter)
+Phone.bind("<FocusOut>",on_leave)
 
 Frame(frame,width=295,height=2,bg='black').place(x=110,y=160)
 
 def on_enter(e):
-    confirmpass.delete(0,'end')
+    gmail.delete(0,'end')
 def on_leave(e):
-    if confirmpass.get()=='':
-        confirmpass.insert(0,'Confirm Password')
+    if gmail.get()=='':
+        gmail.insert(0,'Enter your Gmail')
 
+gmail=Entry(frame,width=25,fg="black",border=0,bg='white',font=('Microsoft Yahei UI Light',13))
+gmail.place(x=110,y=200)
+gmail.insert(0,'Enter your Gmail ')
+gmail.bind("<FocusIn>",on_enter)
+gmail.bind("<FocusOut>",on_leave)
 
-confirmpass=Entry(frame,width=25,fg="black",border=0,bg='white',font=('Microsoft Yahei UI Light',13))
-confirmpass.place(x=110,y=214)
-confirmpass.insert(0,'Confirm Password')
-confirmpass.bind("<FocusIn>",on_enter)
-confirmpass.bind("<FocusOut>",on_leave)
+Frame(frame,width=295,height=2,bg='black').place(x=110,y=230)
 
-Frame(frame,width=295,height=2,bg='black').place(x=110,y=240)
 root.mainloop()
+
